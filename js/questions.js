a7668cd7c090c546c178eabedf3e5be3f1b9554a
davestar <142616037+davevsstudio@users.noreply.github.com>
2024-09-12 21:57:28 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,302 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "An office which is run successfully shows that every member of staff works ___________",
+    answer: "as a team in the office",
+    options: [
+      "in the office",
+      "as a team in the office",
+      "individually in the office",
+      "in group in the office"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following of a not a reward of being truthful?",
+    answer: "bad reputation",
+    options: [
+      "bad reputation",
+      "good reputation",
+      "promotion",
+      "useful suggestion"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The person who is the sole provider of business capital and suffers losses alone is called ",
+    answer: "sole proprietor",
+    options: [
+      "partner ",
+      "trader",
+      "salesman",
+      "sole proprietor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Adeoluwa bought stationary from a bookshop and pays cash. The booksellers will give him",
+    answer: "an invoice",
+    options: [
+      "an invoice",
+      "an order",
+      "a voucher",
+      "a recepit"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The head of the state depatment is?",
+    answer: "purchase manager",
+    options: [
+      "purchase manager",
+      "sales manager",
+      "personnel manager",
+      "cheif manager"
+    ]
+  },
+  {
+    numb: 6,
+    question: "An insurance policy that covers accidents at sea is known as",
+    answer: "marine insurance",
+    options: [
+      "fire insurance",
+      "marine insurance",
+      "life insurance",
+      "burglary  insurance"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "The following are departments in an organziation EXCEPT",
+    answer: "production department",
+    options: [
+      "purchasing department",
+      "sales department",
+      "production department",
+      "organziation department"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "The space allowed at the edge of a document to make it more attractive is called",
+    answer: "orientation",
+    options: [
+      "attraction",
+      "margin ",
+      "design",
+      "orientation"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "A bookseller must process all the foowing qualities EXCEPT",
+    answer: "he must be doubtful in calculation",
+    options: [
+      "he must write clearly",
+      "he must be doubtful in calculation",
+      "he must be intelligent",
+      "he must be accurate in calculation"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "The minimum member you can find in a partnership business is",
+    answer: "seven",
+    options: [
+      "two",
+      "four",
+      "seven",
+      "twenty"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Creating public awareness abot the organization's product is the responsibility of ",
+    answer: "purchasing department",
+    options: [
+      "personnel department",
+      "production department",
+      "marketing department",
+      "purchasing department"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "The book signed by people who vist an office is known as",
+    answer: "visitor's book",
+    options: [
+      "visitor's book",
+      "welcome book",
+      "office book",
+      "time book"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "Insurer in insurance business is the",
+    answer: "risk giver",
+    options: [
+      "insurance company",
+      "agent of the insured",
+      "broker of the insured",
+      "risk giver"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "______ is the activities of buying, selling and distributions of goods and services",
+    answer: "commerce",
+    options: [
+      "transportation",
+      "transformation",
+      "commerce",
+      "warehosuing"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "An introductory card is another name for ",
+    answer: "business card",
+    options: [
+      "business card",
+      "quotation form",
+      "requisition form",
+      "inventory card"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "Which of the following is NOT a consonant?",
+    answer: "O",
+    options: [
+      "O",
+      "P",
+      "B",
+      "T"
+    ]
+  },
+
+
+  {
+    numb: 17,
+    question: "A business organzation with unlimted number of shareholder is known as",
+    answer: "public company",
+    options: [
+      "sole proprietorship",
+      "partnership",
+      "private company",
+      "public company"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "Which of the following is NOT one of the uses of journal?",
+    answer: "recording cash purchases",
+    options: [
+      "correction of error",
+      "transfer between account",
+      "recording cash purchases",
+      "closing entries"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "________ type pf insurance deals with stealing",
+    answer: "burglary",
+    options: [
+      "burglary",
+      "comprehensive",
+      "assurance",
+      "third party"
+    ]
+  },
+
+
+
+  {
+    numb: 20,
+    question: "The method of classifying files are the following EXCEPT ",
+    answer: "sorting",
+    options: [
+      "chronological",
+      "geographical",
+      "alphabetical",
+      "sorting"
+    ]
+  },
+
+
+  {
+    numb: 21,
+    question: "The act of putting typing sheet into the typewriter ready for typing is called paper_________",
+    answer: "insertion",
+    options: [
+      "insertion",
+      "inclusion",
+      "fixing",
+      "placing"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "Production is said to be",
+    answer: "creation of good and services so as to satisfy human want",
+    options: [
+      "changing the form of raw materials",
+      "creation of only services",
+      "creation of goods",
+      "creation of good and services so as to satisfy human want"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "One of the Nigerian International Seasport is in",
+    answer: "lagos",
+    options: [
+      "onitsha",
+      "kano",
+      "ibadan",
+      "lagos"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Petty casbook is another name for",
+    answer: "minor expenses",
+    options: [
+      "bank transaction",
+      "minor expenses",
+      "staff salary",
+      "credit sales"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "A place where correspondence is recevied in the office is called?",
+    answer: "despatch office",
+    options: [
+      "personnel office",
+      "despatch office",
+      "registry",
+      "cash office"
+    ]
+  },
+];
